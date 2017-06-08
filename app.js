63ae0d4359996f8463eb7e3c49d75a7b42f296f5
Garin Wally <garwall101@gmail.com>
2017-06-08 15:18:03 -0600

    Final Draft Prototype of Nhood Atlas Web App

--- app.js
+++ app.js
@@ -1,46 +1,63 @@
 // App
 var app = angular.module("app", ['ngRoute', 'leaflet-directive']);
-console.log("started app");
+console.log("Initialized App");
+
+/* Angular filter to set URLs for nhoods
+    remove '/' 
+    remove spaces (max 6) from nhood names
+    NOTE: Python does it with a regex, here we use replace()
+*/
+app.filter("makeUrl", function() {
+    return function(x) {
+        // 
+        var clean = x.replace("/", "")
+        // Remove spaces (max 6)
+        clean = clean.replace(" ", "").replace(" ", "").replace(" ", "");
+        clean = clean.replace(" ", "").replace(" ", "").replace(" ", "");
+        return "#/nhoods/" + clean;
+    };
+});
 
 
-// Controls views
+// Controls which view is displayed dependant on URL
 app.config(function($routeProvider) {
     // Home
 	$routeProvider.when("/", {
-		templateUrl: 'views/test.html',
+		templateUrl: 'page_views/home.html',
 		controller: 'mainCtrl'
 	});
 
+    // About
     $routeProvider.when("/about", {
-		templateUrl: 'views/about.html',
+		templateUrl: 'page_views/about.html',
 		controller: 'mainCtrl'
 	});
-    
+
+    // Contact
 	$routeProvider.when("/contact", {
-		templateUrl: 'views/contact.html',
+		templateUrl: 'page_views/contact.html',
 		controller: 'mainCtrl'
 	});
     
+    // Help
 	$routeProvider.when("/help", {
-		templateUrl: 'views/404.html',
+		templateUrl: 'page_views/404.html',
 		controller: 'mainCtrl'
 	});
-	
-	$routeProvider.when("/nhood", {
-		templateUrl: 'views/nhood.html',
-		controller: 'mainCtrl'
+    
+    // The page that gets loaded for any/every nhood
+	$routeProvider.when("/nhoods/:nhood", {
+        // The passthrough dynamically loads the selected nhood view
+		templateUrl: "page_views/passthrough.html",
+        controller: "mainCtrl",
+        resolve: function($route) {
+            return $route.current.params.nhood;
+            }
 	});
-    /*
-	$routeProvider.when("/:nhood", {
-		templateUrl: 'views/nhood.html',
-		controller: $routeParams.nhoodCtrl,  // variable holds controller name
-		resolve: function() {
-			nhoodCtrl["nhood"] = $routeParams.nhood;
-		};
-	});
-    */
+
+    // Anything else gets 404'd
 	$routeProvider.otherwise({
-		templateUrl: 'views/404.html'
+		templateUrl: 'page_views/404.html'
 	});
 });
-console.log("started route provider");
+console.log("Initialized route provider");
--- ctrl.js
+++
@@ -1,9 +0,0 @@
-app.controller("mainCtrl", ["$scope", function($scope) {
-    $scope.pageTitle = "Neighborhood Atlas";
-    
-    // Misc info
-	$scope.date = new Date().toLocaleDateString();
-	$scope.contact_name = "Garin Wally";
-	$scope.contact_phone = "555-5555";
-}]);
-console.log("mainCtrl");
